da17c96b0d04e41068de23f007de0fe197db3ab6
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-10-06 14:32:15 -0300

    Update codigoventana.py

--- codigoventana.py
+++ codigoventana.py
@@ -2,16 +2,17 @@ import tkinter as tk
 ventana=tk.Tk()
 ventana.title("Mi ventana")
 ventana.geometry("400x300")
-ventana.mainloop()
-
-def saludar():
-    print("Hola")
-boton= tk.Button(ventana, text="Saludar", command=saludar)
-boton.pack()
 
 label= tk.Label( ventana, text="Hola", font=("Arial", 14))
 label.pack()
 
 entrada=tk.Entry(ventana)
-entrada.pack()   #pide al usuario
+entrada.pack(pady=10)   #pide al usuario
 texto=entrada.get() #obtener el valor
+
+def saludar():
+    print("Hola")
+boton= tk.Button(ventana, text="Saludar", command=saludar)
+boton.pack( )
+
+ventana.mainloop()
